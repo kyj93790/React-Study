2386d618a01e764b4f1931012e51466196260cda
kyj93790 <kyj93790@naver.com>
2021-12-16 18:02:23 +0900

    끝말잇기 Class로 구현 & 웹팩 데브 서버와 핫 리로딩

--- react/week02/webpack.config.js
+++ react/week02/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+// hot reloading -> 기존 데이터를 유지한 채로 수정 가능
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 module.exports = {
 	name: 'wordrelay-setting',
@@ -19,15 +21,34 @@ module.exports = {
 			test: /\.jsx?/,	// js파일과 jsx파일에 룰을 적용하겠다는 것(정규표현식)
 			loader: 'babel-loader', // babel rule을 적용
 			options: {	//babel의 옵션
-				presets: ['@babel/preset-env', '@babel/preset-react'],
-				plugins: ['@babel/plugin-proposal-class-properties'],
+				presets: [
+                    ['@babel/preset-env', {
+                        targets : {
+                            browsers: ['> 1% in KR'], //browserslist
+                        },
+                        debug: true,
+                    }],
+                    '@babel/preset-react',
+                ],
+                plugins: [
+					'@babel/plugin-proposal-class-properties',
+					'react-refresh/babel',
+				],
 			}
 		}],
 	},
-
+	plugins: [
+		new RefreshWebpackPlugin()
+	],
 	output: {
 		// path.join : 경로를 합쳐줌
 		path: path.join(__dirname, 'dist'),
-		filename: 'app.js'
+		filename: 'app.js',
+		publicPath: '/dist/',
 	},	// 출력
+	devServer: {	// 빌드의 결과물을 dist 폴더에 메모리로 저장하는데 여기서 변경점을 찾아 수정해줌
+		devMiddleware: {publicPath: '/dist/'},	// 생성 경로
+		static: {directory: path.resolve(__dirname)},	// index.html 파일의 경로
+		hot: true,
+	},
 };
