2fc81468ce74b05c2077716249ad9d1cbd757d80
kyj93790 <kyj93790@naver.com>
2021-12-17 15:54:09 +0900

    끝말잇기 프로그램 Hooks로 전환

--- react/week02/WordRelay.jsx
+++ react/week02/WordRelay.jsx
@@ -1,56 +1,43 @@
 // 쪼갠 파일에서 필요로 하는 패키지나 라이브러리를 가져옴
 const React = require('react');
 // 아래에서 extends React.Component를 Component로 쓸 수 있게 해줌
-const { Component } = React;
+const { useState, useRef } = React;
 
-class WordRelay extends Component {
-	state = {
-		word: '기차',
-		value: '',
-		result: '',
-	};
+const WordRelay = () => {
+	const [word, setWord] = useState('기차');
+	const [value, setValue] = useState('');
+	const [result, setResult] = useState('');
+	const inputRef = useRef(null);
 
-	// 직접 만드는 함수는 화살표 함수로 구현해야 함
-	onSubmitForm = (e) => {
+	const onSubmitForm = (e) => {
 		e.preventDefault();
-		if (this.state.word[this.state.word.length - 1] === this.state.value[0]){
-			this.setState({
-				result: '딩동댕',
-				word: this.state.value,
-				value: '',
-			})
-			this.input.focus();
+		if (word[word.length - 1] === value[0]){
+			setResult('딩동대앵');
+			setWord(value);
+			setValue('');
+			inputRef.current.focus();
 		}
 		else {
-			this.setState({
-				result: '땡',
-				value: '',
-			})
-			this.input.focus();
+			setResult('땡');
+			setValue('');
+			inputRef.current.focus();
 		}
 	};
 
-	onChangeInput = (e) => {
-		this.setState({value: e.target.value});
+	const onChangeInput = (e) => {
+		setValue(e.target.value);
 	};
 
-	input;
-	onRefInput = (c) => {
-		this.input = c;
-	};
-
-	render() {
-		return (
-			<>
-				<div>{this.state.word}</div>
-				<form onSubmit={this.onSubmitForm}>
-					<input ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput} />
-					<button>입력!</button>
-				</form>
-				<div>{this.state.result}</div>
-			</>
-		);
-	}
+	return (
+		<>
+			<div>{word}</div>
+			<form onSubmit={onSubmitForm}>
+				<input ref={inputRef} value={value} onChange={onChangeInput} />
+				<button>입력!</button>
+			</form>
+			<div>{result}</div>
+		</>
+	);
 }
 
 // 쪼갠 파일에서 쓰는 component를 바깥에서도 사용할 수 있게 해 줌
