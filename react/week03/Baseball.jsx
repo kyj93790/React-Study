23785091c831604e7bd3814c22802eb900d605ee
kyj93790 <kyj93790@naver.com>
2021-12-18 22:21:22 +0900

    숫자야구 구현

--- react/week03/Baseball.jsx
+++ react/week03/Baseball.jsx
@@ -2,37 +2,75 @@ import React, { Component } from 'react';
 import Try from './Try';
 
 function getNumbers() { // 숫자 네 개를 겹치지 않고 랜덤하게 뽑는 함수
-
+    const candidate = [1,2,3,4,5,6,7,8,9];
+    const array = [];
+    for (let i=0; i<4; i+=1) {
+        const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+        array.push(chosen);
+    }
+    return array;
 }
 
 class Baseball extends Component {
     state = {
         result: '',
         value: '',
-        answer: getNumbers(),
-        tries: [],
+        answer: getNumbers(), // ex: [1, 3, 5, 7]
+        tries: [], // push 쓰면 안됨. (참조가 바뀌지 않아서 react가 변화를 감지하지 못함 -> 렌더링이 진행되지 않음)
     };
 
     // 화살표 함수를 쓰지 않으면 this 사용 불가.
     // 화살표 함수를 쓰지 않으면 constructor를 써야 함.
     onSubmitForm = (e) => {
         e.preventDefault();
-        
+        if (this.state.value === this.state.answer.join('')) {
+            this.setState({
+                result: '홈런!',
+                tries: [...this.state.tries, { try: this.state.value, result: '홈런!' }]
+            })
+            alert('게임을 다시 시작합니다!');
+            this.setState({
+                value: '',
+                answers: getNumbers(),
+                tries: [],
+            });
+        }
+        else {
+            const answerArray = this.state.value.split('').map((v) => parseInt(v));
+            let strike = 0;
+            let ball = 0;
+            if (this.state.tries.length >= 9) { // 10번 이상 틀렸을 때
+                this.setState({
+                    result : `10번 넘게 틀려서 실패 ! 답은 ${this.state.answer.join(',')}였습니다!`,
+                });
+                alert('게임을 다시 시작합니다!');
+                this.setState({
+                    value: '',
+                    answers: getNumbers(),
+                    tries: [],
+                });
+            }
+            else {
+                for (let i=0; i<4; i+=1) {
+                    if (answerArray[i] === this.state.answer[i]) {
+                        strike += 1;
+                    } else if (this.state.answer.includes(answerArray[i])) {
+                        ball += 1;
+                    }
+                }
+                this.setState({
+                    tries: [...this.state.tries, {try: this.state.value, result: `${strike} 스트라이크, ${ball} 볼입니다`}],
+                });
+            }
+        }
     };
 
     onChangeInput = (e) => {
-
+        console.log(this.state.answer);
+        this.setState({
+            value: e.target.value,
+        });
     };
-
-    fruits = [
-        { fruit: '사과', taste: '맛있다'},
-        { fruit: '감', taste: '시다'},
-        { fruit: '귤', taste: '달다'},
-        { fruit: '밤', taste: '떫다'},
-        { fruit: '배', taste: '맛있다'},
-        { fruit: '무', taste: '맛있다'},
-        { fruit: '사과', taste: '맛없다'},
-    ];
 
     render() {
         return (
@@ -43,11 +81,11 @@ class Baseball extends Component {
                 </form>
                 <div>시도 : {this.state.tries.length}</div>
                 <ul>
-                    {this.fruits.map((v, i) => {
+                    {this.state.tries.map((v, i) => {
                         return (
                             // props -> 부모 자식 관계가 생김
                             // Baseball 이 Try 의 부모가 됨. (부모가 자식에게 props를 물려줌)
-                            <Try key={v.fruit + v.taste} value={v} index={i} />
+                            <Try key={`${i + 1}차 시도: `} tryInfo={v} />
                         );
                     })}
                 </ul>
